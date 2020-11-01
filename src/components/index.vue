75ad837744bdbfb6430a37e2c1bc367069a10279
Elephant <15968865116@163.com>
2020-11-01 17:39:24 +0800

    点击弹出特效

--- src/components/index.vue
+++ src/components/index.vue
@@ -9,9 +9,12 @@
 <body>
     <div class="bigmark">
         <div class="centermark">
-            <div class="morecentermark"></div>
-            <div class="centermarkright"></div>
-            <div class="centermarkleft"></div>
+            <div ref="bookmark" v-for="bookmarks in bookmarkswihtstyle" :key="bookmarks['bookmark']"
+            @mouseover="beenchose(bookmarks['bookmark'])" @mouseout="beenmove(bookmarks['bookmark'])"
+            :id="bookmarks['bookmark']"
+            :style="bookmarks['style']">
+            <font size="6">我是一段文字</font>
+            </div>
         </div>
         <div class="bottommark"></div>
     </div>
@@ -22,13 +25,102 @@
 export default {
   data () {
     return {
-      bookmark: [1, 2, 3, 4]
+      bookmarkswihtstyle: [
+        {
+          'bookmark': '',
+          'style': '',
+          'title': ''
+        }
+      ]
     }
   },
   mounted () {
-    this.bookmark = [1, 2, 3, 4]
-    var lablelength = 500
-    var bookcount = this.bookmark.length
+    // bookmark由服务器传输过来，大致包括标题、序号，需要转换成标题、序号加样式
+    var bookmarks = [{'bookmark': '1', 'title': 'this is one'},
+      {'bookmark': '2', 'title': 'this is one'},
+      {'bookmark': '3', 'title': 'this is one'},
+      {'bookmark': '4', 'title': 'this is one'},
+      {'bookmark': '5', 'title': 'this is one'},
+      {'bookmark': '6', 'title': 'this is one'},
+      {'bookmark': '7', 'title': 'this is one'},
+      {'bookmark': '8', 'title': 'this is one'},
+      {'bookmark': '9', 'title': 'this is one'},
+      {'bookmark': '10', 'title': 'this is one'}]
+    var blockwidth = 100
+    var lablewidth = 500
+    var lableheight = 400
+    var bookcount = bookmarks.length
+    this.bookmarkswihtstyle = this.thelocation(bookcount, lablewidth, lableheight, blockwidth, bookmarks)
+    console.log(this.bookmarkswihtstyle)
+  },
+  methods: {
+    beenchose: function (bookmark) {
+      document.getElementById(bookmark).style.background = 'red'
+      var height = document.getElementById(bookmark).style.top.slice(0, -1)
+      var newheiht = parseInt(height) - 50
+      document.getElementById(bookmark).style.top = String(newheiht) + 'px'
+      document.getElementById(bookmark).style.zIndex = 9999
+      document.getElementById(bookmark).style.opacity = 1
+    },
+    beenmove: function (bookmark) {
+      document.getElementById(bookmark).style.background = 'green'
+      var height = document.getElementById(bookmark).style.top.slice(0, -1)
+      var newheiht = parseInt(height) + 50
+      document.getElementById(bookmark).style.top = String(newheiht) + 'px'
+      document.getElementById(bookmark).style.zIndex = 1
+      document.getElementById(bookmark).style.opacity = 0.4
+    },
+    thelocation: function (alllenth, lablewidth, lableheight, blockwidth, bookmarks) {
+      var countfir = 0
+      var countsec = 0
+      var countthir = 0
+      var countfor = 0
+      var bookmarkswihtstyle = []
+      var averagewidthbreak = 100 / (alllenth / 4 + 1)
+      var averageheightbreak = 60 / (alllenth / 4 + 1)
+      var locationleft = 0
+      var locationtop = 0
+      var style = 'width: 50px;height:200px;background-color: green;position: absolute;transition: top 3s;z-index:1;opacity:0.4;left:'
+      var newstyle = ''
+      var onesample = {}
+      for (var i = 0; i < bookmarks.length; i++) {
+        switch (i % 4) {
+          case 0:
+            locationleft = lablewidth / 2 - blockwidth / 2 - countfir * averagewidthbreak
+            locationtop = lableheight / 2 + 60 - countfir * averageheightbreak
+            newstyle = style + String(locationleft) + 'px;top:' + String(locationtop) + 'px;'
+            onesample = { 'bookmark': bookmarks[i]['bookmark'], 'style': newstyle, 'title': bookmarks[i]['title'] }
+            bookmarkswihtstyle.push(onesample)
+            countfir = countfir + 1
+            break
+          case 1:
+            locationleft = lablewidth / 2 - blockwidth / 2 - countsec * averagewidthbreak + 100
+            locationtop = lableheight / 2 + countsec * averageheightbreak
+            newstyle = style + String(locationleft) + 'px;top:' + String(locationtop) + 'px;'
+            onesample = { 'bookmark': bookmarks[i]['bookmark'], 'style': newstyle, 'title': bookmarks[i]['title'] }
+            bookmarkswihtstyle.push(onesample)
+            countsec = countsec + 1
+            break
+          case 2:
+            locationleft = lablewidth / 2 - blockwidth / 2 + countthir * averagewidthbreak - 100
+            locationtop = lableheight / 2 - countthir * averageheightbreak
+            newstyle = style + String(locationleft) + 'px;top:' + String(locationtop) + 'px;'
+            onesample = { 'bookmark': bookmarks[i]['bookmark'], 'style': newstyle, 'title': bookmarks[i]['title'] }
+            bookmarkswihtstyle.push(onesample)
+            countthir = countthir + 1
+            break
+          case 3:
+            locationleft = lablewidth / 2 - blockwidth / 2 + countfor * averagewidthbreak
+            locationtop = lableheight / 2 - 60 + countfor * averageheightbreak
+            newstyle = style + String(locationleft) + 'px;top:' + String(locationtop) + 'px;'
+            onesample = { 'bookmark': bookmarks[i]['bookmark'], 'style': newstyle, 'title': bookmarks[i]['title'] }
+            bookmarkswihtstyle.push(onesample)
+            countfor = countfor + 1
+            break
+        }
+      }
+      return bookmarkswihtstyle
+    }
   }
 }
 </script>
