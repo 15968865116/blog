a9f29b2882b9ce436a26191ea662426434dab677
zhujunjiela <15968865116@163.com>
2020-11-03 18:48:02 +0800

    已完成的首首页

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,15 +1,15 @@
 <template>
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>index</title>
-</head>
-<body>
-    <div class="bigmark" id="bigmark">
-      <canvas id="Canvas">您的浏览器不支持Canvas</canvas>
-        <div class="centermark" id="centermark">
+<div class="">
+  <div>
+    <canvas id="Canvas" class="canvasclass" style="z-index:1">您的浏览器不支持Canvas</canvas>
+    <div class="bottommark" id="bottommark" @mousemove="beenchose('bottommark')" @mouseout="beenmove('bottommark')">
+      <font id="font">
+        解
+      </font>
+    </div>
+  </div>
+    <!-- <div class="bigmark" id="bigmark">
+         <div class="centermark" id="centermark">
             <div ref="bookmark" v-for="bookmarks in bookmarkswihtstyle" :key="bookmarks['bookmark']"
             @mouseover="beenchose(bookmarks['bookmark'])" @mouseout="beenmove(bookmarks['bookmark'])"
             :id="bookmarks['bookmark']"
@@ -17,9 +17,8 @@
             </div>
         </div>
         <div class="bottommark"></div>
-    </div>
-</body>
-</html>
+    </div>-->
+</div>
 </template>
 <script>
 export default {
@@ -31,81 +30,151 @@ export default {
           'style': '',
           'title': ''
         }
-      ]
+      ],
+      balllist: []
     }
   },
   mounted () {
     // 初始化画布
-    this.initCanvas()
-    window.addEventListener('click', this.mousemove)
-    // bookmark由服务器传输过来，大致包括标题、序号，需要转换成标题、序号加样式
-    var bookmarks = [{'bookmark': '1', 'title': 'this is one'},
-      {'bookmark': '2', 'title': 'this is one'},
-      {'bookmark': '3', 'title': 'this is one'},
-      {'bookmark': '4', 'title': 'this is one'},
-      {'bookmark': '5', 'title': 'this is one'},
-      {'bookmark': '6', 'title': 'this is one'},
-      {'bookmark': '7', 'title': 'this is one'},
-      {'bookmark': '8', 'title': 'this is one'},
-      {'bookmark': '9', 'title': 'this is one'},
-      {'bookmark': '10', 'title': 'this is one'}]
-    var bookcount = bookmarks.length
-    document.getElementById('centermark').style.left = String(window.innerWidth / 4) + 'px'
-    document.getElementById('centermark').style.width = String(window.innerWidth / 2) + 'px'
-    document.getElementById('bigmark').style.height = String(window.innerHeight) + 'px'
-    this.bookmarkswihtstyle = this.thelocation(bookcount, window.innerWidth / 2, window.innerHeight / 4, window.innerWidth / 10, bookmarks)
+    let canvas = document.getElementById('Canvas')
+    console.log(window.innerHeight)
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight - 5
+    for (var i = 0; i < 200; i++) {
+      var x = Math.random() * canvas.width
+      var y = Math.random() * canvas.height
+      var radius = Math.random() * 10
+      var ball = new this.Ball(x, y, canvas.width / 2, canvas.height / 2, radius, 'white')
+      this.balllist.push(ball)
+    }
+    this.animate()
+    document.getElementById('bottommark').style.left = String(canvas.width / 2 - 40) + 'px'
+    document.getElementById('bottommark').style.top = String(canvas.height / 2 - 40) + 'px'
+    document.getElementById('bottommark').style.cursor = 'pointer'
     window.onresize = () => {
-      this.initCanvas()
-      document.getElementById('centermark').style.left = String(window.innerWidth / 4) + 'px'
-      document.getElementById('centermark').style.width = String(window.innerWidth / 2) + 'px'
-      document.getElementById('bigmark').style.height = String(window.innerHeight) + 'px'
-      this.bookmarkswihtstyle = this.thelocation(bookcount, window.innerWidth / 2, window.innerHeight / 4, window.innerWidth / 10, bookmarks)
+      let canvas = document.getElementById('Canvas')
+      canvas.width = window.innerWidth
+      canvas.height = window.innerHeight - 4
+      document.getElementById('bottommark').style.left = String(canvas.width / 2 - 40) + 'px'
+      document.getElementById('bottommark').style.top = String(canvas.height / 2 - 40) + 'px'
+      document.getElementById('bottommark').style.fontSize = String(40) + 'px'
+      document.getElementById('bottommark').style.cursor = 'pointer'
+      this.balllist = []
+      for (var i = 0; i < 200; i++) {
+        var x = Math.random() * canvas.width
+        var y = Math.random() * canvas.height
+        var radius = Math.random() * 10
+        var ball = new this.Ball(x, y, canvas.width / 2, canvas.height / 2, radius, 'white')
+        this.balllist.push(ball)
+      }
     }
   },
   methods: {
     // 初始化Canvas
     initCanvas: function () {
       let canvas = document.getElementById('Canvas')
-      canvas.width = window.innerWidth - 32
-      canvas.height = window.innerHeight / 2
+      canvas.width = window.innerWidth
+      canvas.height = window.innerHeight - 4
+      // var ctx = canvas.getContext('2d')
+      // for (var i = 0; i < 100; i++) {
+      //   var x = 5 + Math.random() * (canvas.width - 10)
+      //   var y = 5 + Math.random() * (canvas.height - 10)
+      //   var radius = Math.random() * 10
+      //   ctx.arc(x, y, radius, 0, Math.PI * 2, false)
+      // }
+    },
+    // 创建ball对象
+    Ball: function (x, y, destx, desty, radius, color) {
+      let canvas = document.getElementById('Canvas')
+      canvas.width = window.innerWidth
+      canvas.height = window.innerHeight - 4
+      this.sd = Math.random() * 2000
       var ctx = canvas.getContext('2d')
-      for (var i = 0; i < 100; i++) {
-        var x = 5 + Math.random() * (canvas.width - 10)
-        var y = 5 + Math.random() * (canvas.height - 10)
-        var radius = Math.random() * 10
+      this.x = x
+      this.y = y
+      this.destx = destx
+      this.desty = desty
+      this.dx = (this.destx - this.x) / this.sd
+      this.dy = (this.desty - this.y) / this.sd
+      this.radius = radius
+      this.color = color
+      this.draw = function () {
         ctx.beginPath()
-        ctx.arc(x, y, radius, 0, 2 * Math.PI, false)
-        ctx.fillStyle = 'red'
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false)
+        ctx.fillStyle = this.color
         ctx.fill()
         ctx.closePath()
       }
+      this.update = function () {
+        if (((this.destx - 10) <= this.x) && (this.x <= this.destx + 10) && (this.desty - 10 <= this.y) && (this.y <= this.desty + 10)) {
+          this.x = Math.random() * canvas.width
+          this.y = Math.random() * canvas.height
+          this.dx = (this.destx - this.x) / this.sd
+          this.dy = (this.desty - this.y) / this.sd
+        } else {
+          this.x = this.x + this.dx
+          this.y = this.y + this.dy
+        }
+        this.draw()
+      }
+    },
+    drawline: function (x, y, movetox, movetoy) {
+      let canvas = document.getElementById('Canvas')
+      canvas.width = window.innerWidth
+      canvas.height = window.innerHeight - 4
+      var ctx = canvas.getContext('2d')
+      ctx.beginPath()
+      ctx.moveTo(x, y)
+      ctx.lineTo(movetox, movetoy)
+      ctx.stroke()
+      ctx.closePath()
     },
     // loop
     animate: function () {
       requestAnimationFrame(this.animate)
-    },
-    // 鼠标移动事件
-    mousemove: function () {
-      console.log(event.screenX)
-      console.log(event.screenY)
+      let canvas = document.getElementById('Canvas')
+      canvas.width = window.innerWidth
+      canvas.height = window.innerHeight - 4
+      var ctx = canvas.getContext('2d')
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
+      ctx.font = '48px Microsoft YaHei'
+      ctx.fillText('封', canvas.width / 2 - 24, canvas.height / 2 + 16)
+      ctx.beginPath()
+      ctx.arc(canvas.width / 2, canvas.height / 2, 40, 0, Math.PI * 2, false)
+      ctx.stroke()
+      ctx.closePath()
+      ctx.lineWidth = 4
+      ctx.setLineDash([5, 15])
+      ctx.moveTo(canvas.width / 2 + 40, canvas.height / 2)
+      ctx.lineTo(canvas.width / 2 + 240, canvas.height / 2)
+      ctx.stroke()
+      ctx.moveTo(canvas.width / 2 - 40, canvas.height / 2)
+      ctx.lineTo(canvas.width / 2 - 240, canvas.height / 2)
+      ctx.stroke()
+      ctx.moveTo(canvas.width / 2 + 20, canvas.height / 2 - 20 * Math.sqrt(3))
+      ctx.lineTo(canvas.width / 2 + 120, canvas.height / 2 - 100 * Math.sqrt(3))
+      ctx.stroke()
+      ctx.moveTo(canvas.width / 2 - 20, canvas.height / 2 - 20 * Math.sqrt(3))
+      ctx.lineTo(canvas.width / 2 - 120, canvas.height / 2 - 100 * Math.sqrt(3))
+      ctx.stroke()
+      ctx.moveTo(canvas.width / 2 + 20, canvas.height / 2 + 20 * Math.sqrt(3))
+      ctx.lineTo(canvas.width / 2 + 120, canvas.height / 2 + 100 * Math.sqrt(3))
+      ctx.stroke()
+      ctx.moveTo(canvas.width / 2 - 20, canvas.height / 2 + 20 * Math.sqrt(3))
+      ctx.lineTo(canvas.width / 2 - 120, canvas.height / 2 + 100 * Math.sqrt(3))
+      ctx.stroke()
+      for (var i = 0; i < 200; i++) {
+        this.balllist[i].update()
+      }
     },
     // 当前块被选择时 会向上进行弹出
     beenchose: function (bookmark) {
-      document.getElementById(bookmark).style.background = 'red'
-      var height = document.getElementById(bookmark).style.top.slice(0, -1)
-      var newheiht = parseInt(height) - 50
-      document.getElementById(bookmark).style.top = String(newheiht) + 'px'
-      document.getElementById(bookmark).style.zIndex = 9999
-      document.getElementById(bookmark).style.opacity = 1
+      document.getElementById('bottommark').style.opacity = 1
+      document.getElementById('font').style.color = 'purple'
     },
     // 离开当前块时会恢复成原来的样子
     beenmove: function (bookmark) {
-      document.getElementById(bookmark).style.background = 'green'
-      var height = document.getElementById(bookmark).style.top.slice(0, -1)
-      var newheiht = parseInt(height) + 50
-      document.getElementById(bookmark).style.top = String(newheiht) + 'px'
-      document.getElementById(bookmark).style.zIndex = 1
-      document.getElementById(bookmark).style.opacity = 0.4
+      document.getElementById('bottommark').style.opacity = 0
     },
     // 输入一系列的bookmark 返回bookmark的style格式
     thelocation: function (alllenth, lablewidth, lableheight, blockwidth, bookmarks) {
@@ -118,7 +187,7 @@ export default {
       var averageheightbreak = 60 / (alllenth / 4 + 1)
       var locationleft = 0
       var locationtop = 0
-      var style = 'width: 50px;height:200px;background-color: green;position: absolute;transition: top 3s;z-index:1;opacity:0.4;left:'
+      var style = 'width: 50px;height:200px;background-color: green;position: absolute;transition: top 3s;z-index:1;opacity:1;left:'
       var newstyle = ''
       var onesample = {}
       for (var i = 0; i < bookmarks.length; i++) {
@@ -163,31 +232,56 @@ export default {
 }
 </script>
 <style>
+    * {
+      margin:0;
+      padding:0;
+    }
     .bigmark{
         width: 100%;
         height: 100%;
-        background-color: green;
         margin: auto;
         position:relative;
     }
     .centermark{
         width: 500px;
         height: 400px;
-        background-color: yellow;
         position: absolute;
         left:30%;
         top:10px;
+        opacity: 1;
+    }
+    .canvasclass {
+      background-image: url('../assets/naliduo22.jpg');
+      background-repeat:no-repeat;
+      background-size:100% 100%;
     }
     .bottommark{
-        width: 50px;
-        height: 200px;
-        background-color: greenyellow;
+        width: 80px;
+        height: 80px;
+        background-size:100% 100%;
+        float: left;
+        left: 500px;
+        top: 10px;
+        z-index: 2;
         position: absolute;
-        bottom: 0;
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        font-size: 50px;
+        text-align: center;
+        transition: opacity 3s;
+        opacity: 0;
+        font-weight: bold;
     }
     .headerline{
       width: 100%;
       height: 50px;
       background-color:black;
+    }
+    html,body{
+      height: 100%;
+      width:100%;
+    }
+    .fullscree {
+      height: 100%;
+      width:100%;
     }
 </style>
